9f946e9c1dc259ef824f9bb65e2a80b297486060
nazifbara <nazifbarassounon@gmail.com>
2021-09-06 12:26:13 +0100

    update the header to include nav menu

--- src/App.js
+++ src/App.js
@@ -1,18 +1,62 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Switch, Route, Link, useHistory } from 'react-router-dom';
+import { Auth, Hub, DataStore } from 'aws-amplify';
 
 import Home from './Home';
 import Quiz from './Quiz';
 import Dashboard from './Dashboard';
 
 function App() {
+  const [userAuthenticated, setUserAuthenticated] = useState(false);
+  let history = useHistory();
+
+  useEffect(() => {
+    Hub.listen('auth', async (data) => {
+      switch (data.payload.event) {
+        case 'signIn':
+          setUserAuthenticated(true);
+          break;
+        case 'signOut':
+          setUserAuthenticated(false);
+          await DataStore.clear();
+          break;
+        default:
+          break;
+      }
+    });
+    Auth.currentAuthenticatedUser()
+      .then(() => setUserAuthenticated(true))
+      .catch(() => setUserAuthenticated(false));
+
+    return () => {
+      Hub.remove('auth');
+    };
+  }, []);
+
+  function signOut() {
+    Auth.signOut();
+    history.push('/');
+  }
+
   return (
-    <Router>
+    <>
       <header>
         <div className="container container--hp">
-          <Link to="/" className="app-name">
-            QuizApp
-          </Link>
+          <nav>
+            <Link to="/" className="app-name">
+              QuizApp
+            </Link>
+            <div className="menu">
+              <Link className="menu_item" to="/dashboard">
+                Dashboard
+              </Link>
+              {userAuthenticated && (
+                <button className="menu_item logout" onClick={signOut}>
+                  Logout
+                </button>
+              )}
+            </div>
+          </nav>
         </div>
       </header>
       <div className="container container--hp container--vp">
@@ -28,7 +72,7 @@ function App() {
           </Route>
         </Switch>
       </div>
-    </Router>
+    </>
   );
 }
 
